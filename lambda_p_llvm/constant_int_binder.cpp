418e0c4375ca8c46b3f11074cd7f08d81aadf322
clemahieu <clemahieu@gmail.com>
2011-10-04 14:55:08 -0500

    Constant int now takes a bits parameter.

--- lambda_p_llvm/constant_int_binder.cpp
+++ lambda_p_llvm/constant_int_binder.cpp
@@ -12,7 +12,7 @@
 #include <lambda_p/errors/unexpected_node_type.h>
 
 #include <llvm/Constants.h>
-#include <llvm/Type.h>
+#include <llvm/DerivedTypes.h>
 
 #include <wchar.h>
 #include <errno.h>
@@ -30,7 +30,7 @@ lambda_p_llvm::constant_int_binder::~constant_int_binder(void)
 
 void lambda_p_llvm::constant_int_binder::bind (::lambda_p::core::statement * statement, ::std::map < ::lambda_p::core::node *, ::boost::shared_ptr < ::lambda_p::binder::node_instance> > & instances, ::std::vector < ::boost::shared_ptr < ::lambda_p::errors::error> > & problems)	
 {
-	check_count (1, 2, statement, problems);
+	check_count (1, 3, statement, problems);
 	if (problems.empty ())
 	{
 		::lambda_p::core::node * base_node (statement->association->parameters [0]);
@@ -45,7 +45,17 @@ void lambda_p_llvm::constant_int_binder::bind (::lambda_p::core::statement * sta
 				{
 				case ::lambda_p::core::node_data:
 					{
-						parse_nodes (base_node, number_node, statement->association->results [0], instances, problems);
+						::lambda_p::core::node * bits_node (statement->association->parameters [2]);
+						::lambda_p::core::node_id bits_id (bits_node->node_type ());
+						switch (bits_id)
+						{
+						case ::lambda_p::core::node_data:
+							parse_nodes (base_node, number_node, bits_node, statement->association->results [0], instances, problems);
+							break;
+						default:
+							problems.push_back (::boost::shared_ptr < ::lambda_p::errors::error> (new ::lambda_p::errors::unexpected_node_type (binder_name (), 0, bits_id)));
+							break;
+						}
 					}
 					break;
 				default:
@@ -61,7 +71,7 @@ void lambda_p_llvm::constant_int_binder::bind (::lambda_p::core::statement * sta
 	}
 }
 
-void lambda_p_llvm::constant_int_binder::parse_number (unsigned long base, ::std::wstring & number_wstring, ::lambda_p::core::node * declaration_node, ::std::map < ::lambda_p::core::node *, ::boost::shared_ptr < ::lambda_p::binder::node_instance> > & instances, ::std::vector < ::boost::shared_ptr < ::lambda_p::errors::error> > & problems)
+void lambda_p_llvm::constant_int_binder::parse_number (unsigned long base, unsigned long bits, ::std::wstring & number_wstring, ::lambda_p::core::node * declaration_node, ::std::map < ::lambda_p::core::node *, ::boost::shared_ptr < ::lambda_p::binder::node_instance> > & instances, ::std::vector < ::boost::shared_ptr < ::lambda_p::errors::error> > & problems)
 {
 	wchar_t * next;
 	wchar_t const * string (number_wstring.c_str ());
@@ -80,36 +90,51 @@ void lambda_p_llvm::constant_int_binder::parse_number (unsigned long base, ::std
 	}
 	else
 	{
-		::boost::shared_ptr < ::lambda_p_llvm::value> value (new ::lambda_p_llvm::value (::llvm::ConstantInt::get (::llvm::Type::getInt64Ty (context.context), number)));
+		::boost::shared_ptr < ::lambda_p_llvm::value> value (new ::lambda_p_llvm::value (::llvm::ConstantInt::get (::llvm::IntegerType::get (context.context, bits), number)));
 		instances [declaration_node] = value;
 	}
 }
 
-void lambda_p_llvm::constant_int_binder::parse_nodes (::lambda_p::core::node * base_node, ::lambda_p::core::node * number_node, ::lambda_p::core::node * declaration_node, ::std::map < ::lambda_p::core::node *, ::boost::shared_ptr < ::lambda_p::binder::node_instance> > & instances, ::std::vector < ::boost::shared_ptr < ::lambda_p::errors::error> > & problems)
+void lambda_p_llvm::constant_int_binder::parse_nodes (::lambda_p::core::node * base_node, ::lambda_p::core::node * number_node, ::lambda_p::core::node * bits_node, ::lambda_p::core::node * declaration_node, ::std::map < ::lambda_p::core::node *, ::boost::shared_ptr < ::lambda_p::binder::node_instance> > & instances, ::std::vector < ::boost::shared_ptr < ::lambda_p::errors::error> > & problems)
 {
 	::lambda_p::core::data * base_data (static_cast < ::lambda_p::core::data *> (base_node));
 	::lambda_p::core::data * number_data (static_cast < ::lambda_p::core::data *> (number_node));
+	::lambda_p::core::data * bits_data (static_cast < ::lambda_p::core::data *> (bits_node));
 	::std::wstring base_wstring (base_data->string ());
 	::std::wstring number_wstring (number_data->string ());
+	::std::wstring bits_wstring (bits_data->string ());
 	wchar_t * next;
 	wchar_t const * string (base_wstring.c_str ());
 	unsigned long base = ::std::wcstol (string, &next, 10);
 	if (base == 0 && string == next)
 	{
 		::std::wstring message;
-		message.append (L"constant_int_binder was unable to parse base: ");
+		message.append (L"unable to parse base: ");
 		message.append (base_wstring);
 		add_error (message, problems);
 	}
 	else
 	{
-		if (base >= 2 && base <= 36)
+		wchar_t * bits_next;
+		wchar_t const * bits_string (bits_wstring.c_str ());
+		unsigned long bits = ::std::wcstol (bits_string, &bits_next, 10);
+		if (bits == 0 && bits_string == bits_next)
 		{
-			parse_number (base, number_wstring, declaration_node, instances, problems);
+			::std::wstring message;
+			message.append (L"unable to parse bits: ");
+			message.append (bits_wstring);
+			add_error (message, problems);
 		}
 		else
 		{
-			add_error (::std::wstring (L"constant_int_binder has a base that's not between 2 and 36, inclusive"), problems);
+			if (base >= 2 && base <= 36)
+			{
+				parse_number (base, bits, number_wstring, declaration_node, instances, problems);
+			}
+			else
+			{
+				add_error (::std::wstring (L"constant_int_binder has a base that's not between 2 and 36, inclusive"), problems);
+			}
 		}
 	}
 }
--- lambda_p_llvm/constant_int_binder.h
+++ lambda_p_llvm/constant_int_binder.h
@@ -20,8 +20,8 @@ namespace lambda_p_llvm
 		void bind (::lambda_p::core::statement * statement, ::std::map < ::lambda_p::core::node *, ::boost::shared_ptr < ::lambda_p::binder::node_instance> > & instances, ::std::vector < ::boost::shared_ptr < ::lambda_p::errors::error> > & problems);
 		::std::wstring binder_name ();
 	private:
-		void parse_number (unsigned long base, ::std::wstring & number_wstring, ::lambda_p::core::node * declaration_node, ::std::map < ::lambda_p::core::node *, ::boost::shared_ptr < ::lambda_p::binder::node_instance> > & instances, ::std::vector < ::boost::shared_ptr < ::lambda_p::errors::error> > & problems);
-		void parse_nodes (::lambda_p::core::node * base_node, ::lambda_p::core::node * number_node, ::lambda_p::core::node * declaration_node, ::std::map < ::lambda_p::core::node *, ::boost::shared_ptr < ::lambda_p::binder::node_instance> > & instances, ::std::vector < ::boost::shared_ptr < ::lambda_p::errors::error> > & problems);
+		void parse_number (unsigned long base, unsigned long bits, ::std::wstring & number_wstring, ::lambda_p::core::node * declaration_node, ::std::map < ::lambda_p::core::node *, ::boost::shared_ptr < ::lambda_p::binder::node_instance> > & instances, ::std::vector < ::boost::shared_ptr < ::lambda_p::errors::error> > & problems);
+		void parse_nodes (::lambda_p::core::node * base_node, ::lambda_p::core::node * number_node, ::lambda_p::core::node * bits_node, ::lambda_p::core::node * declaration_node, ::std::map < ::lambda_p::core::node *, ::boost::shared_ptr < ::lambda_p::binder::node_instance> > & instances, ::std::vector < ::boost::shared_ptr < ::lambda_p::errors::error> > & problems);
 		::lambda_p_llvm::generation_context & context;
 	};
 }
