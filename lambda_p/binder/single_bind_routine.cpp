94b2c7b94e932f6c394be2b1ec40a2f39699ba60
clemahieu <clemahieu@gmail.com>
2011-10-06 16:06:21 -0500

    Added one-shot class to bind a routine to instances.

---
+++ lambda_p/binder/single_bind_routine.cpp
@@ -0,0 +1,32 @@
+#include "single_bind_routine.h"
+
+#include <lambda_p/core/routine.h>
+#include <lambda_p/core/association.h>
+#include <lambda_p/core/statement.h>
+#include <lambda_p/binder/routine_binder.h>
+#include <lambda_p/binder/routine.h>
+
+lambda_p::binder::single_bind_routine::single_bind_routine (::boost::shared_ptr < ::lambda_p::binder::routine> inner_routine_a, ::boost::shared_ptr < ::lambda_p::binder::routine_instances> inner_instances_a)
+	: binder_m (new ::lambda_p::binder::routine_binder),
+	outer_routine (new ::lambda_p::core::routine),
+	outer_instances (new ::lambda_p::binder::routine_instances)
+{
+	size_t binder_parameter (outer_routine->add_declaration ());
+	outer_routine->surface->results.push_back (binder_parameter);
+	size_t routine_parameter (outer_routine->add_declaration ());
+	outer_routine->surface->results.push_back (routine_parameter);
+	size_t instances_parameter (outer_routine->add_declaration ());
+	outer_routine->surface->results.push_back (instances_parameter);
+	statement = outer_routine->add_statement (binder_parameter);
+	statement->association->parameters.push_back (routine_parameter);
+	statement->association->parameters.push_back (instances_parameter);
+	(*outer_instances) [0] = binder_m;
+	(*outer_instances) [1] = inner_routine_a;
+	(*outer_instances) [2] = inner_instances_a;
+}
+
+
+void lambda_p::binder::single_bind_routine::operator () (::std::vector < ::boost::shared_ptr < ::lambda_p::errors::error> > & problems)
+{
+	binder_m->bind (statement, *outer_instances, problems);
+}
