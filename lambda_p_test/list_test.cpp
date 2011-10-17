a7490a4746f57466edbd60e98dd1d8cb7fbb140f
clemahieu <clemahieu@gmail.com>
2011-10-17 16:08:59 -0500

    Added negative test for list.

--- lambda_p_test/list_test.cpp
+++ lambda_p_test/list_test.cpp
@@ -11,6 +11,7 @@ void lambda_p_test::list_test::run ()
 {
 	run_1 ();
 	run_2 ();
+	run_3 ();
 }
 
 void lambda_p_test::list_test::run_1 ()
@@ -50,4 +51,22 @@ void lambda_p_test::list_test::run_2 ()
 	assert (problems.empty ());
 	assert (routine.instances [result1].get () != NULL);
 	assert (routine.instances [result2].get () != NULL);
+}
+
+void lambda_p_test::list_test::run_3 ()
+{
+	::lambda_p::core::routine routine;
+	size_t list (routine.add_declaration ());
+	routine.surface->results.push_back (list);
+	::lambda_p::core::statement * statement (routine.add_statement (list));
+	size_t result1 (routine.add_declaration ());
+	size_t result2 (routine.add_declaration ());
+	statement->association->results.push_back (result1);
+	statement->association->results.push_back (result2);
+	::boost::shared_ptr < ::lambda_p::binder::list> binder (new ::lambda_p::binder::list);
+	binder->instances.push_back (::boost::shared_ptr < ::lambda_p::binder::instance> (new ::lambda_p::binder::null_binder));
+	routine.instances [list] = binder;
+	::std::vector < ::boost::shared_ptr < ::lambda_p::errors::error> > problems;
+	binder->bind (statement, routine.instances, problems);
+	assert (!problems.empty ());
 }
