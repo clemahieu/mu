b7e0ef4fcf0721397e312f8b482233da2ec84e6d
clemahieu <clemahieu@gmail.com>
2012-01-09 12:42:05 -0600

    Fixed reference test.

--- lambda_p_script_io_test/call.cpp
+++ lambda_p_script_io_test/call.cpp
@@ -3,6 +3,7 @@
 #include <lambda_p_script_io/call.h>
 #include <lambda_p_script/call.h>
 #include <lambda_p_script/expression.h>
+#include <lambda_p_script/reference.h>
 #include <lambda_p/node.h>
 #include <lambda_p/parameters.h>
 #include <lambda_p/reference.h>
@@ -41,13 +42,17 @@ void lambda_p_script_io_test::call::run_2 ()
 
 void lambda_p_script_io_test::call::run_3 ()
 {
-	//std::map <boost::shared_ptr <lambda_p::call>, size_t> reservations;
-	//boost::shared_ptr <lambda_p_script::call> target (new lambda_p_script::call (1));
-	//boost::shared_ptr <lambda_p::parameters> parameters (new lambda_p::parameters);
-	//boost::shared_ptr <lambda_p::reference> reference (new lambda_p::reference);
-	//lambda_p_script_io::call call (reservations, target, parameters);
-	//assert (target->arguments.size () == 1);
-	//auto added (boost::dynamic_pointer_cast <lambda_p_script::expression> (target->arguments [0]));
-	//assert (added.get () != nullptr);
-	//assert (added->index == 0);
+	std::map <boost::shared_ptr <lambda_p::expression>, size_t> reservations;
+	boost::shared_ptr <lambda_p_script::call> target (new lambda_p_script::call (1));
+	boost::shared_ptr <lambda_p::parameters> parameters (new lambda_p::parameters);
+	reservations.insert (std::map <boost::shared_ptr <lambda_p::expression>, size_t>::value_type (parameters, 0));
+	boost::shared_ptr <lambda_p::reference> reference (new lambda_p::reference);
+	reference->expression = parameters;
+	reference->index = 0;
+	lambda_p_script_io::call call (reservations, target, reference);
+	assert (target->arguments.size () == 1);
+	auto added (boost::dynamic_pointer_cast <lambda_p_script::reference> (target->arguments [0]));
+	assert (added.get () != nullptr);
+	assert (added->expression == 0);
+	assert (added->index == 0);
 }
