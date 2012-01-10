52bdf1b333bd2019c63996594734dc069b6cfe13
clemahieu <clemahieu@gmail.com>
2012-01-10 16:14:33 -0600

    Added package.

---
+++ lambda_p_script/operations/package/add.cpp
@@ -0,0 +1,48 @@
+#include "add.h"
+
+#include <lambda_p_script/operations/package.h>
+#include <lambda_p_script/nodes/data.h>
+#include <lambda_p/errors/error_target.h>
+
+#include <sstream>
+
+void lambda_p_script::operations::package::add::operator () (boost::shared_ptr <lambda_p::errors::error_target> errors_a, std::vector <boost::shared_ptr <lambda_p::node>> & parameters, std::vector <boost::shared_ptr <lambda_p::node>> & results)
+{
+	auto expected (check_count (errors_a, parameters, 3));
+	if (expected)
+	{
+		auto one (boost::dynamic_pointer_cast <class lambda_p_script::operations::package> (parameters [0]));
+		auto two (boost::dynamic_pointer_cast <lambda_p_script::nodes::data> (parameters [1]));
+		if (one.get () != nullptr)
+		{
+			if (two.get () != nullptr)
+			{
+				auto existing (one->items.find (two->string));
+				if (existing != one->items.end ())
+				{
+					one->items.insert (std::map <std::wstring, boost::shared_ptr <lambda_p::node>>::value_type (two->string, parameters [2]));
+				}
+				else
+				{
+					std::wstringstream message;
+					message << L"Package already has an item named: ";
+					message << two->string;
+					(*errors_a) (message.str ());
+				}
+			}
+			else
+			{
+				invalid_type (errors_a, 1);
+			}
+		}
+		else
+		{
+			invalid_type (errors_a, 0);
+		}
+	}
+}
+
+std::wstring lambda_p_script::operations::package::add::name ()
+{
+	return std::wstring (L"lambda_p_script::operations::package::add");
+}
---
+++ lambda_p_script/operations/package/add.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <lambda_p_script/nodes/operation.h>
+
+namespace lambda_p_script
+{
+	namespace operations
+	{
+		namespace package
+		{
+			class add : public lambda_p_script::nodes::operation
+			{
+			public:
+				void operator () (boost::shared_ptr <lambda_p::errors::error_target> errors_a, std::vector <boost::shared_ptr <lambda_p::node>> & parameters, std::vector <boost::shared_ptr <lambda_p::node>> & results) override;
+				std::wstring name () override;
+			};
+		}
+	}
+}
+
