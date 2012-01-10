3381a8730d969604edc79e9b2e62059f378b99db
clemahieu <clemahieu@gmail.com>
2012-01-10 12:42:29 -0600

    Added tests.

--- lambda_p_script_io_test/builder.cpp
+++ lambda_p_script_io_test/builder.cpp
@@ -11,6 +11,8 @@ void lambda_p_script_io_test::builder::run ()
 {
 	run_1 ();
 	run_2 ();
+	run_3 ();
+	run_4 ();
 }
 
 void lambda_p_script_io_test::builder::run_1 ()
@@ -38,4 +40,31 @@ void lambda_p_script_io_test::builder::run_2 ()
 	assert (builder.errors->errors.empty ());
 	assert (results.size () == 1);
 	assert (results [0] == n2);
+}
+
+void lambda_p_script_io_test::builder::run_3 ()
+{
+	lambda_p_script_io::builder builder;
+	lambda_p_io::source source (boost::bind (&lambda_p_io::lexer::lexer::operator(), &builder.lexer, _1));
+	source (L"[:~]");
+	auto routine (builder.routines [0]);
+	std::vector <boost::shared_ptr <lambda_p::node>> arguments;
+	std::vector <boost::shared_ptr <lambda_p::node>> results;
+	auto n1 (boost::shared_ptr <lambda_p_script::operations::identity> (new lambda_p_script::operations::identity));
+	arguments.push_back (n1);
+	(*routine) (builder.errors, arguments, results);
+	assert (builder.errors->errors.empty ());
+	assert (results.size () == 0);
+}
+
+void lambda_p_script_io_test::builder::run_4 ()
+{
+	lambda_p_script_io::builder builder;
+	lambda_p_io::source source (boost::bind (&lambda_p_io::lexer::lexer::operator(), &builder.lexer, _1));
+	source (L"[:~]");
+	auto routine (builder.routines [0]);
+	std::vector <boost::shared_ptr <lambda_p::node>> arguments;
+	std::vector <boost::shared_ptr <lambda_p::node>> results;
+	(*routine) (builder.errors, arguments, results);
+	assert (!builder.errors->errors.empty ());
 }
