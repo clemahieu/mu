2947acd8c35f5aa531f890a8a66299ac34017572
clemahieu <clemahieu@gmail.com>
2012-01-17 12:31:14 -0600

    Added link_modules.

---
+++ lambda_p_llvm_test/linker/link_modules.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace lambda_p_llvm_test
+{
+	namespace linker
+	{
+		class link_modules
+		{
+		public:
+			void run ();
+			void run_1 ();
+		};
+	}
+}
+
--- lambda_p_llvm_test/tests.cpp
+++ lambda_p_llvm_test/tests.cpp
@@ -2,6 +2,7 @@
 
 #include <lambda_p_llvm_test/module/get_package.h>
 #include <lambda_p_llvm_test/module/add_package.h>
+#include <lambda_p_llvm_test/linker/link_modules.h>
 
 void lambda_p_llvm_test::tests::run ()
 {
@@ -11,6 +12,10 @@ void lambda_p_llvm_test::tests::run ()
 	}
 	{
 		lambda_p_llvm_test::module::add_package test;
+		test.run ();
+	}
+	{
+		lambda_p_llvm_test::linker::link_modules test;
 		test.run ();
 	}
 }
