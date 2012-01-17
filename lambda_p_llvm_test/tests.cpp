9a7040ebfb07517412e882db55c838fec474f07e
clemahieu <clemahieu@gmail.com>
2012-01-17 13:24:50 -0600

    Added linking example. Added module create.

--- lambda_p_llvm_test/tests.cpp
+++ lambda_p_llvm_test/tests.cpp
@@ -4,8 +4,15 @@
 #include <lambda_p_llvm_test/module/add_package.h>
 #include <lambda_p_llvm_test/linker/link_modules.h>
 
+#include <llvm/LLVMContext.h>
+#include <llvm/Module.h>
+
 void lambda_p_llvm_test::tests::run ()
 {
+	{
+		llvm::LLVMContext context;
+		llvm::Module (llvm::StringRef (), context);
+	}
 	{
 		lambda_p_llvm_test::module::get_package test;
 		test.run ();
