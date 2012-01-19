ac578364b5043df3e99235b7ee25d2b25f6044fa
clemahieu <clemahieu@gmail.com>
2012-01-18 21:16:21 -0600

    Added to compile operation. Added to compile example.

--- lambda_p_llvm/compile/operation.cpp
+++ lambda_p_llvm/compile/operation.cpp
@@ -60,7 +60,7 @@ void lambda_p_llvm::compile::operation::operator () (boost::shared_ptr <lambda_p
 				if (link)
 				{
 					std::stringstream command;
-					command << "link /NOLOGO /ENTRY:main ";
+					command << "\"c:\\Program Files (x86)\\Microsoft Visual Studio 10.0\\VC\\vcvarsall.bat\" amd64 && link kernel32.lib /NOLOGO /SUBSYSTEM:WINDOWS /ENTRY:_start ";
 					command << obj_name;
 					command << " /OUT:";
 					command << name->string;
