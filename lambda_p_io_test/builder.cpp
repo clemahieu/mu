d36e8f8ee914bfd589a6eb35eed4c0abc9caadfe
clemahieu <clemahieu@gmail.com>
2012-01-23 23:04:54 -0600

    Added context tests for builder.

--- lambda_p_io_test/builder.cpp
+++ lambda_p_io_test/builder.cpp
@@ -28,6 +28,8 @@ void lambda_p_io_test::builder::run_1 ()
 	assert (routine->body->dependencies.size () == 1);
 	assert (routine->body->dependencies [0] == routine->parameters);
 	assert (routine->parameters->dependencies.size () == 0);
+	assert (routine->parameters->context == lambda_p::context (1, 1, 0, 1, 1, 0));
+	assert (routine->body->context == lambda_p::context (1, 1, 0, 1, 4, 3));
 }
 
 void lambda_p_io_test::builder::run_2 ()
@@ -38,6 +40,12 @@ void lambda_p_io_test::builder::run_2 ()
 	source (L"[:~]");
 	assert (builder.errors->errors.empty ());
 	assert (builder.routines.size () == 2);
+	auto routine1 (builder.routines [0]);
+	auto routine2 (builder.routines [1]);
+	assert (routine1->parameters->context == lambda_p::context (1, 1, 0, 1, 1, 0));
+	assert (routine1->body->context == lambda_p::context (1, 1, 0, 1, 4, 3));
+	assert (routine2->parameters->context == lambda_p::context (1, 5, 4, 1, 5, 4));
+	assert (routine2->body->context == lambda_p::context (1, 5, 4, 1, 8, 7));
 }
 
 void lambda_p_io_test::builder::run_3 ()
@@ -48,15 +56,18 @@ void lambda_p_io_test::builder::run_3 ()
 	assert (builder.errors->errors.empty ());
 	assert (builder.routines.size () == 1);
 	auto routine (builder.routines [0]);
+	assert (routine->body->context == lambda_p::context (1, 1, 0, 1, 25, 24));
 	assert (routine->body->dependencies.size () == 3);
 	auto d1 (boost::dynamic_pointer_cast <lambda_p::reference> (routine->body->dependencies [0]));
 	assert (d1.get () != nullptr);
 	auto d2 (boost::dynamic_pointer_cast <lambda_p::expression> (routine->body->dependencies [1]));
 	assert (d2.get () != nullptr);
+	assert (d2->context == lambda_p::context (1, 16, 15, 1, 22, 21));
 	auto d3 (boost::dynamic_pointer_cast <lambda_p::reference> (routine->body->dependencies [2]));
 	assert (d3.get () != nullptr);
 	auto d11 (boost::dynamic_pointer_cast <lambda_p::expression> (d1->expression));
 	assert (d11.get () != nullptr);
+	assert (d11->context == lambda_p::context (1, 2, 1, 1, 12, 11));
 	assert (d1->index == 0);
 	assert (d11->dependencies.size () == 1);
 	assert (d11->dependencies [0] == routine->parameters);
