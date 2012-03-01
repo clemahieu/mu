f5f7e761c8523204c4344dd5bd866e2071bc7c65
clemahieu <clemahieu@gmail.com>
2012-03-01 15:51:48 -0600

    Extracting core.

--- mu/script/load/operation.cpp
+++ mu/script/load/operation.cpp
@@ -24,47 +24,10 @@ void mu::script::load::operation::operator () (boost::shared_ptr <mu::core::erro
 	{
 		if (file.get () != nullptr)
 		{
-			auto path (boost::filesystem::initial_path ());
-			path /= std::string (file->string.begin (), file->string.end ());
-			std::ifstream stream;
-			stream.open (path.string ());
-			if (stream.is_open ())		
+			auto result (core (errors_a, extensions, file));
+			if (result.get () != nullptr)
 			{
-				auto input (boost::shared_ptr <mu::io::lexer::istream_input> (new mu::io::lexer::istream_input (stream)));
-				mu::script_io::builder builder (extensions->extensions);
-				mu::io::source source (boost::bind (&mu::io::lexer::lexer::operator(), &builder.lexer, _1));
-				source (input);
-				source ();
-				if (builder.errors->errors.empty ())
-				{
-					if (builder.clusters.size () == 1)
-					{
-						results.push_back (builder.clusters [0]);
-					}
-					else
-					{
-						std::wstringstream message;
-						message << L"File did not contain one cluster: ";
-						message << builder.clusters.size ();
-						(*errors_a) (message.str ());
-					}
-				}
-				else
-				{
-					for (auto i (builder.errors->errors.begin ()), j (builder.errors->errors.end ()); i != j; ++i)
-					{
-						(*errors_a) ((*i).first, (*i).second);
-					}
-				}
-			}
-			else
-			{
-				std::wstringstream message;
-				message << L"File could not be opened: ";
-				std::string patha (path.string ());
-				std::wstring path (patha.begin (), patha.end ());
-				message << path;
-				(*errors_a) (message.str ());
+				results.push_back (result);
 			}
 		}
 		else
@@ -81,4 +44,52 @@ void mu::script::load::operation::operator () (boost::shared_ptr <mu::core::erro
 size_t mu::script::load::operation::count ()
 {
 	return 2;
+}
+
+boost::shared_ptr <mu::script::cluster::node> mu::script::load::operation::core (boost::shared_ptr <mu::core::errors::error_target> errors_a, boost::shared_ptr <mu::script::extensions::node> extensions, boost::shared_ptr <mu::script::string::node> file)
+{
+	boost::shared_ptr <mu::script::cluster::node> result;
+	auto path (boost::filesystem::initial_path ());
+	path /= std::string (file->string.begin (), file->string.end ());
+	std::ifstream stream;
+	stream.open (path.string ());
+	if (stream.is_open ())		
+	{
+		auto input (boost::shared_ptr <mu::io::lexer::istream_input> (new mu::io::lexer::istream_input (stream)));
+		mu::script_io::builder builder (extensions->extensions);
+		mu::io::source source (boost::bind (&mu::io::lexer::lexer::operator(), &builder.lexer, _1));
+		source (input);
+		source ();
+		if (builder.errors->errors.empty ())
+		{
+			if (builder.clusters.size () == 1)
+			{
+				result = builder.clusters [0];
+			}
+			else
+			{
+				std::wstringstream message;
+				message << L"File did not contain one cluster: ";
+				message << builder.clusters.size ();
+				(*errors_a) (message.str ());
+			}
+		}
+		else
+		{
+			for (auto i (builder.errors->errors.begin ()), j (builder.errors->errors.end ()); i != j; ++i)
+			{
+				(*errors_a) ((*i).first, (*i).second);
+			}
+		}
+	}
+	else
+	{
+		std::wstringstream message;
+		message << L"File could not be opened: ";
+		std::string patha (path.string ());
+		std::wstring path (patha.begin (), patha.end ());
+		message << path;
+		(*errors_a) (message.str ());
+	}
+	return result;
 }
--- mu/script/load/operation.h
+++ mu/script/load/operation.h
@@ -6,12 +6,25 @@ namespace mu
 {
 	namespace script
 	{
+		namespace string
+		{
+			class node;
+		}
+		namespace extensions
+		{
+			class node;
+		}
+		namespace cluster
+		{
+			class node;
+		}
 		namespace load
 		{
 			class operation : public mu::script::fixed
 			{
 			public:
 				void operator () (boost::shared_ptr <mu::core::errors::error_target> errors_a, mu::core::segment <boost::shared_ptr <mu::core::node>> parameters, std::vector <boost::shared_ptr <mu::core::node>> & results) override;
+				boost::shared_ptr <mu::script::cluster::node> core (boost::shared_ptr <mu::core::errors::error_target> errors_a, boost::shared_ptr <mu::script::extensions::node> extensions, boost::shared_ptr <mu::script::string::node> file);
 				size_t count () override;
 			};
 		}
