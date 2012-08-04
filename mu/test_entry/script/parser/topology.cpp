2b2121884f0ff72ea84cc654eef244314923128e
clemahieu <clemahieu@gmail.com>
2012-08-04 08:47:54 -0500

    Added tests for topology.

--- mu/test_entry/script/parser/topology.cpp
+++ mu/test_entry/script/parser/topology.cpp
@@ -7,74 +7,86 @@
 
 #include <gc_cpp.h>
 
+// Topology of one
 TEST (script_test, topology1)
 {
     mu::core::errors::error_list errors;
 	mu::script::runtime::expression root;
     std::vector <mu::script::runtime::expression *> expressions;
 	mu::script::parser::topology topology (&root,
-        [&expressions]
-        (mu::script::runtime::expression * expression_a)
-        {
-            expressions.push_back (expression_a);
-        },
-        errors);
+                                           [&expressions]
+                                           (mu::script::runtime::expression * expression_a)
+                                            {
+                                                expressions.push_back (expression_a);
+                                            },
+                                            errors);
+    EXPECT_TRUE (!errors ());
     ASSERT_TRUE (expressions.size () == 1);
     EXPECT_TRUE (expressions [0] == &root);
 }
-/*
+
+// Simple nested
 TEST (script_test, topology2)
 {
-	auto routine (new (GC) mu::core::routine);
-	auto root (new (GC) mu::core::expression);
-	routine->body = root;
-	auto c1 (new (GC) mu::core::expression);
-	root->dependencies.push_back (c1);
-	c1->dependencies.push_back (new (GC) mu::core::parameters);
-	auto c2 (new (GC) mu::core::expression);
-	root->dependencies.push_back (c2);
-	c2->dependencies.push_back (c1);
-	mu::script::topology::core order (routine->body);
-	auto i (order.topology->expressions.begin ());
-	EXPECT_EQ (*i, c1);
-	++i;
-	EXPECT_EQ (*i, c2);
-	++i;
-	EXPECT_EQ (*i, root);
-	++i;
-	EXPECT_EQ (i, order.topology->expressions.end ());
-	EXPECT_EQ (order.acyclic, true);
+    mu::core::errors::error_list errors;
+	mu::script::runtime::expression root;
+    mu::script::runtime::expression e1;
+    root.dependencies.push_back (&e1);
+    std::vector <mu::script::runtime::expression *> expressions;
+	mu::script::parser::topology topology (&root,
+                                           [&expressions]
+                                           (mu::script::runtime::expression * expression_a)
+                                           {
+                                               expressions.push_back (expression_a);
+                                           },
+                                           errors);
+    EXPECT_TRUE (!errors ());
+    ASSERT_TRUE (expressions.size () == 2);
+    EXPECT_TRUE (expressions [0] == &e1);
+    EXPECT_TRUE (expressions [1] == &root);
 }
 
+// Double nest
 TEST (script_test, topology3)
 {
-	auto routine (new (GC) mu::core::routine);
-	auto root (new (GC) mu::core::expression);
-	routine->body = root;
-	auto parameters (new (GC) mu::core::expression);
-	parameters->dependencies.push_back (new (GC) mu::core::parameters);
-	auto r1 (new (GC) mu::core::reference (parameters, 0));
-	root->dependencies.push_back (r1);
-	mu::script::topology::core order (routine->body);
-	auto i (order.topology->expressions.begin ());
-	EXPECT_EQ (*i, parameters);
-	++i;
-	EXPECT_EQ (*i, root);
-	++i;
-	EXPECT_EQ (i, order.topology->expressions.end ());
-	EXPECT_EQ (order.acyclic, true);
+    mu::core::errors::error_list errors;
+	mu::script::runtime::expression root;
+    mu::script::runtime::expression e1;
+    mu::script::runtime::expression e2;
+    root.dependencies.push_back (&e1);
+    e1.dependencies.push_back (&e2);
+    std::vector <mu::script::runtime::expression *> expressions;
+	mu::script::parser::topology topology (&root,
+                                           [&expressions]
+                                           (mu::script::runtime::expression * expression_a)
+                                           {
+                                               expressions.push_back (expression_a);
+                                           },
+                                           errors);
+    EXPECT_TRUE (!errors ());
+    ASSERT_TRUE (expressions.size () == 3);
+    EXPECT_TRUE (expressions [0] == &e2);
+    EXPECT_TRUE (expressions [1] == &e1);
+    EXPECT_TRUE (expressions [2] == &root);
 }
 
+// Cycle
 TEST (script_test, topology4)
 {
-	auto routine (new (GC) mu::core::routine);
-	auto root (new (GC) mu::core::expression);
-	routine->body = root;
-	auto expression1 (new (GC) mu::core::expression);
-	auto expression2 (new (GC) mu::core::expression);
-	expression1->dependencies.push_back (expression2);
-	expression2->dependencies.push_back (expression1);
-	root->dependencies.push_back (expression1);
-	mu::script::topology::core core (routine->body);
-	EXPECT_EQ (!core.acyclic, true);
-}*/
+    mu::core::errors::error_list errors;
+	mu::script::runtime::expression root;
+    mu::script::runtime::expression e1;
+    mu::script::runtime::expression e2;
+    root.dependencies.push_back (&e1);
+    e2.dependencies.push_back (&e1);
+    e1.dependencies.push_back (&e2);
+    std::vector <mu::script::runtime::expression *> expressions;
+	mu::script::parser::topology topology (&root,
+                                           [&expressions]
+                                           (mu::script::runtime::expression * expression_a)
+                                           {
+                                               expressions.push_back (expression_a);
+                                           },
+                                           errors);
+    EXPECT_TRUE (errors ());
+}
