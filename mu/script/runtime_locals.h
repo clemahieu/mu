e3492ae9bc50af9a2a9a4c22ca5ae6b73cb0e63b
clemahieu <clemahieu@gmail.com>
2012-08-31 12:41:10 -0500

    Using mu containers.

--- mu/script/runtime_locals.h
+++ mu/script/runtime_locals.h
@@ -2,9 +2,6 @@
 
 #include <mu/core/node.h>
 
-#include <vector>
-#include <map>
-
 #include <boost/tuple/tuple.hpp>
 
 namespace mu
@@ -19,8 +16,8 @@ namespace mu
 			public:
                 locals (size_t parameters_a);
                 size_t parameters;
-				std::map <mu::script::runtime::expression *, boost::tuple <size_t, size_t>> expressions;
-				std::vector <mu::core::node *> frame;
+				mu::map <mu::script::runtime::expression *, boost::tuple <size_t, size_t>> expressions;
+				mu::vector <mu::core::node *> frame;
 			};
 		}
 	}
